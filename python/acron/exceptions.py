22e57365e9767a16c47ef94d5949ddccae238400
Rodrigo Bermudez Schettino <rodrigobermudezschettino@gmail.com>
2021-06-08 14:08:15 +0200

    Headers: Add personal name and email to edited files
    Skipped files, whose header is already updated on PRs #3, #4, and #5.

--- python/acron/exceptions.py
+++ python/acron/exceptions.py
@@ -10,9 +10,9 @@
 '''Exception handling submodule'''
 
 __author__ = 'Philippe Ganz (CERN)'
-__credits__ = ['Philippe Ganz (CERN)', 'Ulrich Schwickerath (CERN)']
-__maintainer__ = 'Philippe Ganz (CERN)'
-__email__ = 'philippe.ganz@cern.ch'
+__credits__ = ['Philippe Ganz (CERN)', 'Ulrich Schwickerath (CERN)', 'Rodrigo Bermudez Schettino (CERN)']
+__maintainer__ = 'Rodrigo Bermudez Schettino (CERN)'
+__email__ = 'rodrigo.bermudez.schettino@cern.ch'
 __status__ = 'Development'
 
 
