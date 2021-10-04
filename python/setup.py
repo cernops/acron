82e5644d6b9136ce17c5d978fc7363a1e799f246
Ulrich Schwickerath <Ulrich.Schwickerath@cern.ch>
2021-10-04 12:34:53 +0200

    Update SELinux rules and improve log rotation

--- python/setup.py
+++ python/setup.py
@@ -13,7 +13,7 @@ from setuptools import setup
 
 
 setup(name='acron',
-      version='0.14.3',
+      version='0.14.4',
       description='Authenticated crontab service',
       author='Philippe Ganz (CERN)',
       author_email='philippe.ganz@cern.ch',
